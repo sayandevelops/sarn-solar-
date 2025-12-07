643b47f7cef3d0f718b40a76307536fe2c997e19
Sayan Mondal <sayon8023@gmail.com>
2025-12-07 09:59:14 +0000

    next iso - https://i.pinimg.com/736x/4c/9b/4d/4c9b4d020a7d1767a9d0256287

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   devIndicators: {
